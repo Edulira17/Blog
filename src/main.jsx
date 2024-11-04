03a5c3da276aa7afcf38ee8224f894461747d719
Eduardo Lira <eduardoliracunha17@gmail.com>
2024-11-04 14:57:04 -0400

    feat: adding POST request and adding new post functionality

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 // pages
 import Home from "./routes/Home.jsx";
 import NewPost from "./routes/NewPost.jsx";
+import Post from "./routes/Post.jsx";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
       {
         path: '/new',
         element: <NewPost/>
+      },
+      // acessar rota individualmente:
+      {
+        path: "/posts/:id",
+        element: <Post/>
       }
     ]
   }
